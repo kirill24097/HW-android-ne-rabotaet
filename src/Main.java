bed87ddb92d5df209669a080564b5db6b26d0eb9
Pavilion <kirill24-974@mail.ru>
2022-07-31 17:11:31 +0300

    домашка условные операторы

---
+++ src/Main.java
@@ -0,0 +1,92 @@
+import java.time.Year;
+
+public class Main {
+  public static void main(String[] args) {
+/// 1 задание
+///      int clientOS = 0;
+///        int clieAndroid = 1;
+///        if(clieAndroid == 1) ;
+///     {
+///            System.out.println("Установите версию приложения для Android по ссылке.");
+///        }
+
+///        if (clientOS == 0) ;
+///        {
+///         System.out.println("Установите версию приложения для iOS по ссылке.");}
+
+///2задание
+
+///        int clientOs = 0;
+///        int Year = 2015;
+///        if (clientOs == 0 && Year < 2016) ;
+///        {
+///            System.out.println("Установите версию лайт.");
+///        }
+
+///        if (clientOs == 0 && Year >= 2015) ;
+///        {
+///            System.out.println("Установите версию норм.");
+///        }
+///        if (clientOs != 0 && Year < 2016) ;
+///        {
+///            System.out.println("Установите версию лайт.");
+///        }
+///        if (clientOs != 0 && Year >= 2015) ;
+///        {
+///            System.out.println("Установите версию норм.");
+///        }
+
+
+///          3 заданре
+
+///        int YearNecessary = 1905;
+///        if (YearNecessary % 4 == 0 && YearNecessary % 100 != 0 || YearNecessary % 400 == 0) ;
+///        {
+///            System.out.println(" високосный.");
+///        }
+///        if (YearNecessary % 4 != 0) ;
+///        {
+///            System.out.println(" не високосный.");
+///      }
+///задание 4
+///        int deliveryDistrance = 10;
+///        int days = 0;
+///        if (deliveryDistance < 20) {
+///            System.out.println("доставка 1 день.");
+///            days++;
+///        } else if (deliveryDistance >= 60 && deliveryDistance < 100) {
+///            System.out.println("доставка 3 день.");
+///           days = days + 3;
+///            }else {
+///                System.out.println("доставка нет.");
+///            }
+
+/// задание 5
+//            int mountNumber = 123;
+///          switch (mountNumber) {
+///                case 12:
+///                case 1:
+///                case 2:
+///                System.out.println ( "зима.");
+///                   break;
+///                case 3:
+///               case 4:
+///                case 5:
+///                System.out.println ( "весна.");
+///                    break;
+///                case 6:
+///                case 7:
+///               case 8:
+///                System.out.println ( "лето.");
+///                    break;
+///                case 9:
+///                case 10:
+///               case 11:
+///                System.out.println ( "осень.");
+///                    break;
+///                default:
+///                    System.out.println("нет такого.");}
+
+
+    }
+}
